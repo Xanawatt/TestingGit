2e1782530ff916ef4833e414d3d847246df1f381
Xanawatt <schneidermark2000@gmail.com>
2016-10-20 17:02:24 -0400

    Revert "Revert "Initial commit""
    This reverts commit be3ca488afdad75a072ddfff85b2aefbfa3307bd.

--- src/org/usfirst/frc1024/Test/Robot.java
+++ src/org/usfirst/frc1024/Test/Robot.java
@@ -160,10 +160,6 @@ public class Robot extends IterativeRobot {
 		//SmartDashboard.putNumber("Gyrospeed", RobotMap.gyrospeed.getRate());
 		SmartDashboard.putNumber("Ultra", RobotMap.Ultra.getRangeInches());
 		SmartDashboard.putNumber("Ping", RobotMap.Ultrasonic.getVoltage());
-		//I made a change here!
-		
-		
-		//I made more changes!!!
 	}
 
 	/**
