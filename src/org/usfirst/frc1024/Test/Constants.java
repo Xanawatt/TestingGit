77ecba9f77106f77b4c4db42e7ed4dd2e8652a4b
Xanawatt <schneidermark2000@gmail.com>
2016-10-27 20:01:26 -0400

    Started PID

---
+++ src/org/usfirst/frc1024/Test/Constants.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc1024.Test;
+
+public class Constants {
+	public final static double Lkp = 0.0;
+	public final static double Lki = 0.0;
+	public final static double Lkd = 0.0;
+	public final static double Lkf = 0.0;
+	
+	public final static double Rkp = 0.0;
+	public final static double Rki = 0.0;
+	public final static double Rkd = 0.0;
+	public final static double Rkf = 0.0;
+}
--- src/org/usfirst/frc1024/Test/commands/Movement.java
+++ src/org/usfirst/frc1024/Test/commands/Movement.java
@@ -2,14 +2,20 @@ package org.usfirst.frc1024.Test.commands;
 
 import org.usfirst.frc1024.Test.Robot;
 import org.usfirst.frc1024.Test.RobotMap;
-import org.usfirst.frc1024.Test.subsystems.drivetrain;
+import org.usfirst.frc1024.Test.subsystems.driveleft;
+import org.usfirst.frc1024.Test.subsystems.driveright;
 
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Movement {
+	
+	public static final driveleft left = new driveleft();
+	public static final driveright right = new driveright();
+	
 	public static void drive(double leftPower, double rightPower){
-		drivetrain.left.set(leftPower);
-		drivetrain.right.set(rightPower);
+		driveleft.left.set(leftPower);
+		driveright.right.set(rightPower);
 	}
 	public static void driveForFeet(int feet, double power){
 		int ticks = (int) ((500*feet)/Math.PI);
@@ -23,15 +29,20 @@ public class Movement {
 		Timer.delay(time);
 		Movement.drive(0, 0);
 	}
-	public static void driveStraightInches(int inches, double power){
+	/*public static void driveStraightInches(int inches, double power){
+		RobotMap.gyro.reset();
+		power = -power;
+		RobotMap.leftEncoder.reset();
+		RobotMap.rightEncoder.reset();
 		int ticks = (int) ((500*inches)/Math.PI)*12;
 		while(RobotMap.leftEncoder.getDistance() < ticks){
 			double angle = RobotMap.gyro.getAngle();
-			RobotMap.myRobot.drive(power, -angle * Robot.kp);
-			Timer.delay(0.004);
+			Movement.drive(power - RobotMap.gyro.getAngle() / 10, power + RobotMap.gyro.getAngle() / 10);
+			SmartDashboard.putNumber("Gyro", angle);
 		}
-		RobotMap.myRobot.drive(0, 0);
-	}
+		RobotMap.myRobot.drive(0, 0); 
+	}*/
+	
 	public static void driveToAngle(double angle, double power){
 		double gyroAngle = RobotMap.gyro.getAngle();
 		while(angle != gyroAngle){
@@ -59,4 +70,24 @@ public class Movement {
 		double leftRear = power * Math.cos(angle + (Math.PI/4)) + rotationSpeed;
 		double rightRear = power * Math.sin(angle + (Math.PI/4)) - rotationSpeed;
 	}
+	
+	public static void pidDrive(double distance){
+		left.pidWrite(distance);
+		right.pidWrite(distance);
+		left.pid.enable();
+		right.pid.enable();
+	}
+	public static void pidStopDrive(){
+		left.pidWrite(0.0);
+		right.pidWrite(0.0);
+		left.pid.enable();
+		right.pid.enable();
+	}
+	
+	
+	
+	
+	
+	
+	
 }
